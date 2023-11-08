36765b16e9728d9535150cb6e8f412c6137a6930
Caio Vinicius <caiocvam3@gmail.com>
2023-11-08 16:41:14 -0300

    Average and pre-order

--- EDB2/ABB/src/BST.java
+++ EDB2/ABB/src/BST.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class BST {
     public Node root;
     public int max_level;
+    public int number_of_nodes;
     
     public void insert(Node node, int n) {
     	if(n > node.value) {
@@ -34,6 +35,10 @@ public class BST {
     	}
     }
     
+    public void calculateNumberOfNodes() {
+    	number_of_nodes =  1 + root.qtd_LNodes + root.qtd_RNodes;
+    }
+    
     public int calculateMax_Level(Node node, int n) {
     	int max = n;
     	if(node.left != null) {
@@ -59,53 +64,45 @@ public class BST {
 		}
 	}
     
-    public Node search(Node node, int key, int mode) {
-    	if(node != null) {
-    		if(node.value == key) {
-    			mode = 1;
-    		}
-    		else {
-    			if(key < node.value) {
-    				if(node.left == null) {
-    					mode = 2;
-    				}
-    				else {
-    					node = node.left;
-    				}
+    //root, unknown, true
+    public boolean search(Node node, int key, boolean bool) {
+    	if(node.value != key) {
+    		if(key < node.value) {
+    			if(node.left != null) {
+    				bool = search(node.left, key, bool);
     			}
     			else {
-    				if(node.right == null) {
-    					mode = 2;
-    				}
-    				else {
-    					node = node.right;
-    				}
+    				return false;
     			}
-    			if(mode < 1) {
-    				search(node, key, mode);
+    		}
+    		else {
+    			if(node.right != null) {
+    				bool = search(node, key, bool);
+    			}
+    			else {
+    				return false;
     			}
     		}
     	}
-    	return node;
+    	return bool;
     }
     
     public int findNthElement(Node node, Integer n, ArrayList<Integer> count) {
-    	Integer sum = root.qtd_LNodes + 1 + root.qtd_RNodes;
-    	if(n > sum) {
+    	if(n > number_of_nodes) {
     		return -1;
     	}
     	int key = -1;
 		if(node.left != null) {
     		key = findNthElement(node.left, n, count);
     	}
-		System.out.println(count);
-    	System.out.println(node.value);
+		//System.out.println(count);
+    	//System.out.println(node.value);
     	if(key != -1) {
     		return key;
     	}
 		count.set(0, count.get(0) + 1);
 		if(count.get(0) == n) {
-			System.out.println("Achou");
+			//System.out.println("Achou");
 			return node.value;
 		}
     	if(node.right != null) {
@@ -120,9 +117,6 @@ public class BST {
     		if(node.left != null) {
     			pos.set(0, findPosition(node.left, key, pos));
         	}
-    		if(key == node.value) {
-    			return pos.get(0);
-    		}
     		pos.set(0, pos.get(0) + 1);
         	if(node.right != null) {
         		pos.set(0, findPosition(node.right, key, pos));
@@ -135,6 +129,26 @@ public class BST {
     	return pos.get(0);
     }
     
+    public int average() {
+    	ArrayList<Integer> count = new ArrayList<Integer>();
+		count.add(0);
+		int n;
+		if(number_of_nodes == 1) {
+			return root.value;
+		}
+    	if(number_of_nodes % 2 == 1) {
+    		return findNthElement(root, number_of_nodes/2, count);
+    	}
+    	if(findNthElement(root, number_of_nodes/2, count) < findNthElement(root, number_of_nodes/2 + 1, count)) {
+    		n = findNthElement(root, number_of_nodes/2, count);
+    	}
+    	else {
+    		n = findNthElement(root, number_of_nodes/2 + 1, count);
+    	}
+    	return n;
+    }
+    
+    //root, true
     public boolean ItIsFull(Node node, boolean bool) {
     	if(node.left != null) {
     		bool = ItIsFull(node.left, bool);
@@ -147,7 +161,7 @@ public class BST {
     	}
     	return bool;
     }
-    
+    //root, true
     public boolean ItIsFlooding(Node node, boolean bool) {
     	if(node.left != null) {
 			bool = ItIsFlooding(node.left, bool);
@@ -159,6 +173,25 @@ public class BST {
 			bool = ItIsFlooding(node.right, bool);
 		}
 		return bool;
+    }
+    
+    //root, count.get(0) = 0, ""
+    public String pre_order(Node node, ArrayList<Integer> count, String order) {
+    	count.set(0, count.get(0) + 1);
+    	if(count.get(0) != number_of_nodes) {
+    		order = order + Integer.toString(node.value) + " -> " ;
+    	}
+    	else {
+    		order = order + Integer.toString(node.value);
+    	}
+    	
+    	if(node.left != null) {
+    		order = pre_order(node.left, count, order);
+    	}
+    	if(node.right != null) {
+    		order = pre_order(node.right, count, order);
+    	}
+    	return order;
     }
     
     private void printFormat1(int m, Node node, int n) {
